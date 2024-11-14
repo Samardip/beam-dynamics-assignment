8d7ab8c8c81fbf9a369db87bc578e1f98dce0cbc
Samardip Mandal <samardipmandal.kgp@gmail.com>
2024-11-14 22:07:14 +0530

    changed player images

--- src/component/showPlayerStats/showPlayerStats.tsx
+++ src/component/showPlayerStats/showPlayerStats.tsx
@@ -8,7 +8,7 @@ export const ShowPlayerStats = ({ selectedPlayer }: any) => {
                 {/* {isImageLoaded ? ( */}
                 <img
                     className='min-h-[300px] h-[75%] w-[65%] opacity-[0.7] object-contain'
-                    src={selectedPlayer?.playerImg}
+                    src={selectedPlayer?.position==='GoalKeeper'?'../assests/test-player-2.png':'test-player.png'}
                     alt="player"
                 />
                 {/* ) : (
